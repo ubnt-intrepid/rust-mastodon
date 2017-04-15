a03efc7c2f032ef92149db334798672ffc12ea1a
Yusuke Sasaki <ubnt-intrepid@users.noreply.github.com>
2017-04-15 20:23:40 +0900

    authentication (#1)

--- src/bin/mstdn.rs
+++ src/bin/mstdn.rs
@@ -1,16 +1,22 @@
 extern crate mastodon;
 extern crate clap;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
 
+use std::fs::OpenOptions;
+use std::io::Write;
+use mastodon::{Mastodon, MastodonConfig};
+use mastodon::register::{App, AppConfig};
+
+#[allow(dead_code)]
 fn register_app() {
-  use mastodon::register::AppConfig;
   let app = AppConfig::new("https://pawoo.net", "rustydon")
     .redirect_uris("urn:ietf:wg:oauth:2.0:oob")
-    .scopes("read")
+    .scopes("read write follow")
     .register()
     .unwrap();
 
-  use std::fs::OpenOptions;
-  use std::io::Write;
   OpenOptions::new()
     .write(true)
     .create(true)
@@ -20,12 +26,34 @@ fn register_app() {
     .unwrap();
 }
 
-fn main() {
-  use mastodon::register::App;
 
-  //register_app();
-  let app: App = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/app.json"))
-    .parse()
+fn main() {
+  let ref matches = clap::App::new("mstdn")
+    .arg_from_usage("--register 'Register App'")
+    .get_matches();
+  if matches.is_present("register") {
+    register_app();
+    return;
+  }
+
+  let f = OpenOptions::new()
+    .read(true)
+    .open(std::env::home_dir().unwrap().join("mastodon.json"))
     .unwrap();
-  println!("{:?}", app);
+
+  #[derive(Deserialize)]
+  struct Config {
+    username: String,
+    password: String,
+    server: String,
+    app: App,
+  }
+  let c: Config = serde_json::from_reader(f).unwrap();
+  let ref username = c.username;
+  let ref password = c.password;
+
+  let config = MastodonConfig::new(c.server, c.app.client_id, c.app.client_secret);
+  let cli = Mastodon::new(config)
+    .and_then(|cli| cli.authenticate(username, password))
+    .unwrap();
 }
